4164ff35e6b8646262e953f3fb162d609c44862a
Sumizome(Mac) <sumizome0213@gmail.com>
2019-02-23 13:54:34 +0900

    [add] nixietunes

---
+++ nixietunes/main.py
@@ -0,0 +1,18 @@
+import appscript
+from time import sleep
+import requests
+
+while True:
+    url = None
+    try:
+        time = appscript.app('iTunes').player_position()
+        if(type(time) is float):
+            url = 'http://192.168.179.7/setting?num=%02d:%02d:%02d' % (time/3600, (time%3600)/60, (time%60)/1)
+        else:
+            url = 'http://192.168.179.7/setting?num=00:00:00'
+    except Exception as ex:
+        print(str(ex))
+    
+    #response = requests.get(url)
+    print(url)
+    sleep(0.1)
