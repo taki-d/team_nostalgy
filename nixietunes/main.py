0d21318fab5ee152e8791b563b21a6370eea5546
Sumizome(Mac) <sumizome0213@gmail.com>
2019-02-23 16:33:33 +0900

    [update] dotを追加

--- nixietunes/main.py
+++ nixietunes/main.py
@@ -7,9 +7,9 @@ while True:
     try:
         time = appscript.app('iTunes').player_position()
         if(type(time) is float):
-            url = 'http://192.168.179.7/setting?num=%02d:%02d:%02d' % (time/3600, (time%3600)/60, (time%60)/1)
+            url = 'http://192.168.179.7/setting?num=%02d:%02d:%02d&dot=00100100' % (time/3600, (time%3600)/60, (time%60)/1)
         else:
-            url = 'http://192.168.179.7/setting?num=00:00:00'
+            url = 'http://192.168.179.7/setting?num=00:00:00&dot=00100100'
     except Exception as ex:
         print(str(ex))
     
